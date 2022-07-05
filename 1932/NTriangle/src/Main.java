1db992cf2b8787a1a8b961573cfb8234a231d772
Martinel2 <kkuldangi3@naver.com>
2022-07-05 13:03:31 +0900

    tri

---
+++ 1932/NTriangle/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+//1932 Á¤¼ö»ï°¢Çü
+public class Main {
+
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int[][] tri = new int[n][n];
+        int[][] dp = new int[n][n];
+		String[] s;
+		for(int i = 1; i<=n; i++)
+		{
+			s = br.readLine().split(" ");
+			for(int j = 0; j<i; j++)
+			{
+				tri[i-1][j] = Integer.parseInt(s[j]);
+			}
+		}
+
+        for(int i = n-1; i>=0; i--){
+
+            for(int j = 0; j<=i; j++){
+                if(i == n-1)
+                    dp[i][j] = tri[i][j];
+                else{
+                    dp[i][j] = Math.max(dp[i+1][j],dp[i+1][j+1])+tri[i][j];
+                }
+            }
+        }
+
+		System.out.println(dp[0][0]);
+	}
+
+}
