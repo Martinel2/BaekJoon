8a988907c5d6064f9ee087b5c8ec708d4bd80e12
Martinel2 <kkuldangi3@naver.com>
2022-08-10 16:44:31 +0900

    max heap

---
+++ 11279/maxheap/src/maxheap/Main.java
@@ -0,0 +1,34 @@
+package maxheap;
+import java.io.*;
+import java.util.*;
+
+//11279 √÷¥Î»¸
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i<N; i++)
+		{
+			int data = Integer.parseInt(br.readLine());
+			
+			if(data == 0)
+			{
+				if(!pq.isEmpty()) {
+					sb.append(pq.remove() + "\n");
+				}
+				else
+					sb.append("0\n");
+			}
+			else
+			{
+				pq.add(data);
+			}
+		}
+		System.out.print(sb);
+		
+	}
+}
