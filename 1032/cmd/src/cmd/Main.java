260036e457cf20fd13d6729a49c90bdb26b28716
Martinel2 <kkuldangi3@naver.com>
2022-08-19 09:08:45 +0900

    8/19

---
+++ 1032/cmd/src/cmd/Main.java
@@ -0,0 +1,27 @@
+package cmd;
+
+import java.io.*;
+import java.util.*;
+
+//2357 ÃÖ¼Ú°ª°ú ÃÖ´ñ°ª
+public class Main {
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String s = "";
+		char[] ans = new char[N];
+		
+		ans = br.readLine().toCharArray();
+		for(int i = 0; i<N-1; i++)
+		{
+			s = br.readLine();
+			for(int j = 0; j<s.length(); j++)
+			{
+				if(ans[j] != s.charAt(j) )
+					ans[j] = '?';
+			}
+		}
+		System.out.print(ans);
+	}
+}
