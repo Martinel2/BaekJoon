3fd988f8a363c29c00ddcd027eb5ac0855fb72ae
Martinel2 <kkuldangi3@naver.com>
2023-02-08 09:14:25 +0900

    2/8

--- 3000~/3986/good word/src/Main.java
+++ 3000~/3986/good word/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Stack;
 
 //3986 좋은 단어
 public class Main {
@@ -9,15 +10,17 @@ public class Main {
 		int ans = 0;
 		for(int i = 0; i<N; i++) {
 			String s = br.readLine();
-			while(true) {
-				if(!s.contains("AA") && !s.contains("BB"))
-					break;
-				if(s.contains("AA"))
-					s = s.replaceAll("AA", "");
-				if(s.contains("BB"))
-					s = s.replaceAll("BB", "");
+			Stack<Character> stack = new Stack<>();
+			for(int j = 0; j<s.length(); j++) {
+				char word = 'C';
+				if(!stack.isEmpty())
+					word = stack.peek();
+				if(word == s.charAt(j))
+					stack.pop();
+				else
+					stack.add(s.charAt(j));
 			}
-			if(s.isEmpty())
+			if(stack.isEmpty())
 				ans++;
 		}
 		System.out.println(ans);
