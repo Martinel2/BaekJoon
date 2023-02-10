06eb3b227e2514b8bfeedb89e1fb81f004194195
Martinel2 <kkuldangi3@naver.com>
2023-02-10 11:11:12 +0900

    2/10

---
+++ 1000~/1309/zoo/src/zoo/Main.java
@@ -0,0 +1,21 @@
+package zoo;
+import java.io.*;
+
+//1309 µ¿¹°¿ø
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if(N==1)
+			System.out.println(3);
+		else {
+			int[] dp = new int[N+1];
+			dp[1] = 3; dp[2] = 7;
+			for(int i = 3; i<=N; i++) {
+				dp[i] = (dp[i-1]*2 + dp[i-2])%9901;
+			}
+			System.out.println(dp[N]);
+		}
+		
+	}
+}
