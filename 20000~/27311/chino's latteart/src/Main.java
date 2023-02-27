7c81f47d97f6d70049bc03df2c0ed1ade0c98d02
Martinel2 <kkuldangi3@naver.com>
2023-02-27 13:47:06 +0900

    2/27

--- 20000~/27311/chino's latteart/src/Main.java
+++ 20000~/27311/chino's latteart/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+		
 		int T = Integer.parseInt(br.readLine());
 		for(int t = 0; t<T; t++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -16,9 +17,9 @@ public class Main {
 			for(int i = 0; i<R; i++) {
 				arr[i] = br.readLine().toCharArray();
 			}
-			
+			//#로 감싸지는 최대의 사각형을 구함
 			int maxx=0,maxy=0;
-			int minx=Integer.MAX_VALUE, miny = Integer.MAX_VALUE;
+			int minx=100, miny = 200;
 			boolean flag = true;
 			for(int i = 0; i<R; i++) {
 				for(int j = 0; j<C; j++) {
@@ -34,10 +35,12 @@ public class Main {
 					}
 				}
 			}
+			//그 사각형이 정사각형이면
 			if(maxx-minx == maxy-miny) {
 				//System.out.println("1clear");
+				//.으로 이루어진 정사각형이 존재하는지 확인
 				int Mxx = 0, Mxy = 0;
-				int Mnx = Integer.MAX_VALUE, Mny = Integer.MAX_VALUE;
+				int Mnx = 100, Mny = 200;
 				for(int i = minx; i<=maxx; i++) {
 					for(int j = miny; j <=maxy; j++) {
 						if(arr[i][j] == '.') {
@@ -52,10 +55,12 @@ public class Main {
 						}
 					}
 				}
-				if(Mxx - Mnx == Mxy - Mny) {
-					//System.out.println("2clear");
-					for(int i = Mnx; i<Mnx; i++) {
-						for(int j = Mny; j < Mxy; j++) {
+				//.으로 이루어진 정사각형이  "구석에" 존재한다면
+				if( (Mxx - Mnx == Mxy - Mny) && ((Mxx == minx || Mnx == minx) || (Mxx == maxx || Mnx == maxx)) 
+						&& ((Mxy == miny || Mny == miny) || (Mxy == maxy || Mny == maxy))) {
+						//그 사각형 안에 #이 있는지 확인
+					for(int i = Mnx; i<=Mxx; i++) {
+						for(int j = Mny; j <=Mxy; j++) {
 							if(arr[i][j] == '#') {
 								flag = false;
 								break;
