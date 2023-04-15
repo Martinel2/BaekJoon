34d2d738d40d02040de7538474a2bb7b7b50c7bb
Martinel2 <kkuldangi3@naver.com>
2023-04-15 09:00:00 +0900

    4/15

---
+++ 20000~/24723/green tower/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int ans = (int)Math.pow(2, N);
+		System.out.println(ans);
+	}
+}
