c949b45e028edc14d31c47b742cc3f46181b09e2
Martinel2 <kkuldangi3@naver.com>
2023-04-27 09:01:15 +0900

    4/27

---
+++ 20000~/27323/rectangle/src/rectangle/Main.java
@@ -0,0 +1,15 @@
+package rectangle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int w = Integer.parseInt(br.readLine());
+		int h = Integer.parseInt(br.readLine());
+		
+		System.out.println(w*h);
+	}
+}
