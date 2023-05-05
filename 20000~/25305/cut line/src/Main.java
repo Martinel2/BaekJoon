da060161856b4e9ef00d8c698f59b914e46f9d1d
Martinel2 <kkuldangi3@naver.com>
2023-05-05 09:00:00 +0900

    5/5

---
+++ 20000~/25305/cut line/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int N,k;
+		N = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[N-k]);
+	}
+}
