e48016b1ea28024f1ec4fc39e1f40a9559b76c5c
Martinel2 <kkuldangi3@naver.com>
2023-06-01 09:09:58 +0900

    6/1

---
+++ 10000~/10822/plus/src/plus/Main.java
@@ -0,0 +1,20 @@
+package plus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] s = br.readLine().split(",");
+		int sum = 0;
+		for(int i = 0; i<s.length; i++) {
+			sum += Integer.parseInt(s[i]);
+		}
+		
+		System.out.println(sum);
+	}
+}
