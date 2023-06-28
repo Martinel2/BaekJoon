cd1b196bc802b492bcad9a4196d60b772c59aa64
Martinel2 <kkuldangi3@naver.com>
2023-06-28 09:00:00 +0900

    6/28

---
+++ 10000~/10158/ant/src/ant/Main.java
@@ -0,0 +1,31 @@
+package ant;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//10158
+public class Main {
+	
+	static int dr[] = {1,1,-1,-1};
+	static int dc[] = {1,-1,-1,1};
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int q = Integer.parseInt(st.nextToken()); //q가 컬럼
+		int p = Integer.parseInt(st.nextToken()); //p가 로우
+		int t = Integer.parseInt(br.readLine());
+		int x = (q+t)%(2*W);
+		int y = (p+t)%(2*H);
+		
+		x = W - Math.abs(W-x);
+		y = H - Math.abs(H-y);
+		
+		System.out.println(x+" "+y);
+	}
+
+}
