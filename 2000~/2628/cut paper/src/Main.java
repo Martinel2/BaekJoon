090bf910366321a3dba7f9fe75153ce5a467ba0c
Martinel2 <kkuldangi3@naver.com>
2023-08-13 09:00:00 +0900

    8/13

--- 2000~/2628/cut paper/src/Main.java
+++ 2000~/2628/cut paper/src/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+//2628
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
