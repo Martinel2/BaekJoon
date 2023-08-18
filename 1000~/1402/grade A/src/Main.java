2906dc4884c5325d1af6f5ca4eb6201259ce9091
Martinel2 <kkuldangi3@naver.com>
2023-08-18 15:48:27 +0900

    8/19

---
+++ 1000~/1402/grade A/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0; t<T; t++) {
+			br.readLine();
+			sb.append("yes\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
