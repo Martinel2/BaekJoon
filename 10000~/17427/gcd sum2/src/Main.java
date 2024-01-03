ada8deacfce1820b78c0338c874462c524dbdfde
Martinel2 <kkuldangi3@naver.com>
2024-01-03 09:28:35 +0900

    1/3

---
+++ 10000~/17427/gcd sum2/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		long M = 0;
+		
+		for(int i = 1; i<=n; i++) {
+			long cd = i * (n/i);
+			
+			M += cd;
+		}
+		
+		System.out.println(M);
+	}
+}
