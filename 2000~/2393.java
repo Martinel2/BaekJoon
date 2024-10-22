3a478678839e15ee383c652e3c1eb2ef0625e7a1
Martinel2 <kkuldangi3@naver.com>
2024-10-22 11:51:11 +0900

    10/22

---
+++ 2000~/2393.java
@@ -0,0 +1,28 @@
+package solved;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+       System.out.println("  ___  ___  ___");
+       System.out.println("  | |__| |__| |");
+       System.out.println("  |           |");
+       System.out.println("   \\_________/");
+       System.out.println("    \\_______/");
+       System.out.println("     |     |");
+       System.out.println("     |     |");
+       System.out.println("     |     |");
+       System.out.println("     |     |");
+       System.out.println("     |_____|");
+       System.out.println("  __/       \\__");
+       System.out.println(" /             \\");
+      System.out.println("/_______________\\");
+    }
+}
