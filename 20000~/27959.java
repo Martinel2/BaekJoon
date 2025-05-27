d7e685c7210fe6f55e89eb42fcb41b809450fa29
Martinel2 <kkuldangi3@naver.com>
2025-05-28 01:39:58 +0900

    5/28

---
+++ 20000~/27959.java
@@ -0,0 +1,20 @@
+package solved;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		String ans = n*100 >= m? "Yes":"No";
+		System.out.println(ans);
+		
+	}
+}
